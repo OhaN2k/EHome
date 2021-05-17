9de1bc321b887678ddd71bdc1d9d8df03208496f
Nam <66206021+OhaN2k@users.noreply.github.com>
2021-05-17 22:59:45 +0700

    added more topic subcriptions

--- e_home/backend/mqtt.py
+++ e_home/backend/mqtt.py
@@ -4,11 +4,10 @@ from Adafruit_IO import *
 
 conn = ConnectDB("ehome.db")
 conn.create_table()
-conn.log_light()
 
 ADAFRUIT_IO_USERNAME = "Ohan"
-ADAFRUIT_IO_KEY = "aio_YUUj11sBFfZNygLRWvLygfR0SbJG"
-topics = ["sound", "light"]
+ADAFRUIT_IO_KEY = "aio_Xdzo29tz5l6N5SFckUXaquhaAdGA"
+topics = ["led", "temperature", "sound", "light", "infrared", "time"]
 
 def connected(client):
     for feed in topics:
@@ -23,42 +22,17 @@ def disconnected(client):
     sys.exit(1)
 
 def message(client, feed_id, payload):
-    if feed_id == "sound":
-        sound_mes(payload)
-        conn.sound_entry(payload)
-    elif feed_id == "light":
-        light_mes(payload)
-        conn.light_entry(payload)
+    if feed_id in topics:
+        date = str(datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S"))
+        print("Received message on topic {} with value {} at {}.".format(feed_id, payload, date))
+        conn.data_entry(feed_id, date, payload)
     else:
         print("Undefined feed")
-
-def sound_mes(payload):
-    data = aio.receive('sound')
-    print("Received message on topic sound {} at {}.".format(payload, data.created_at))
-    if int(payload) > 80:
-        print("Warning! Loud volume")
-    
-
-def light_mes(payload):
-    data = aio.receive('light')
-    print("Received message on topic light {} at {}.".format(payload, data.created_at))
-    if int(payload) < 20:
-        print("Warning! Inefficient light")
-    elif int(payload) > 80:
-        print("Warning! Ambient too bright")
-
-
-
-# data['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__module__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmul__', '__setattr__', '__sizeof__', '__slots__', '__str__', '__subclasshook__', '_asdict', '_field_defaults', '_fields', '_fields_defaults', '_make', '_replace', 
-# 'completed_at', 'count', 'created_at', 'created_epoch', 'ele', 'expiration', 'feed_id', 'from_dict', 'id', 'index', 'lat', 'lon', 'position', 'updated_at', 'value']
-    
-
+        return
 
 # Create an MQTT client instance.
 client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
-aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
-
- 
+# aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
 # Setup the callback functions defined above.
 client.on_connect    = connected
@@ -73,7 +47,5 @@ try:
 except:
     print("Can't connect")
     sys.exit(1)
-
-
 
 client.loop_blocking()
--- e_home/backend/sql.py
+++ e_home/backend/sql.py
@@ -8,52 +8,141 @@ class ConnectDB:
         self.c = self.conn.cursor()
 
     def create_table(self):
-        self.c.execute("CREATE TABLE IF NOT EXISTS database(datestamp TEXT, topic TEXT, value INT)")
+        self.c.execute("CREATE TABLE IF NOT EXISTS data(topic TEXT, datestamp TEXT, value TEXT)")
 
-    def sound_entry(self, payload):
-        unix = time.time()
-        date = str(datetime.datetime.fromtimestamp(unix).strftime("%Y-%m-%d %H:%M:%S"))
-        topic = "sound"
-        self.c.execute("INSERT INTO database (datestamp, topic, value) VALUES(?, ?, ?)",
-                        (date, topic, payload))
+    def data_entry(self, feed_id, date, payload):
+        self.c.execute("INSERT INTO data (topic, datestamp, value) VALUES(?, ?, ?)",
+                        (feed_id, date, payload))
         self.conn.commit()
     
-    def light_entry(self, payload):
-        unix = time.time()
-        date = str(datetime.datetime.fromtimestamp(unix).strftime("%Y-%m-%d %H:%M:%S"))
-        topic = "light"
-        self.c.execute("INSERT INTO database (datestamp, topic, value) VALUES(?, ?, ?)",
-                        (date, topic, payload))
-        self.conn.commit()
+    def read_led(self):
+        self.c.execute("""
+                        SELECT *
+                        FROM data
+                        WHERE topic = 'led'
+                        ORDER BY datestamp DESC
+                        """)
+                        
+        try:
+            data = self.c.fetchone()[1:]
+        except TypeError as err:
+            data = None
+            print(err)
+
+        return data
     
+    def read_temperature(self):
+        self.c.execute("""
+                        SELECT *
+                        FROM data
+                        WHERE topic = 'temperature'
+                        ORDER BY datestamp DESC
+                        """)
+                        
+        try:
+            data = self.c.fetchone()[1:]
+        except TypeError as err:
+            data = None
+            print(err)
+
+        return data
+
     def read_sound(self):
         self.c.execute("""
                         SELECT *
-                        FROM database
+                        FROM data
                         WHERE topic = 'sound'
                         ORDER BY datestamp DESC
                         """)
-        print(self.c.fetchone())
+
+        try:
+            data = self.c.fetchone()[1:]
+        except TypeError as err:
+            data = None
+            print(err)
+
+        return data
 
     def read_light(self):
         self.c.execute("""
                         SELECT *
-                        FROM database
-                        WHERE topic = 'sound'
+                        FROM data
+                        WHERE topic = 'light'
                         ORDER BY datestamp DESC
                         """)
-        print(self.c.fetchone())
+                        
+        try:
+            data = self.c.fetchone()[1:]
+        except TypeError as err:
+            data = None
+            print(err)
+
+        return data
+    
+    def read_infrared(self):
+        self.c.execute("""
+                        SELECT *
+                        FROM data
+                        WHERE topic = 'infrared'
+                        ORDER BY datestamp DESC
+                        """)
+                        
+        try:
+            data = self.c.fetchone()[1:]
+        except TypeError as err:
+            data = None
+            print(err)
+
+        return data
+    
+    def read_time(self):
+        self.c.execute("""
+                        SELECT *
+                        FROM data
+                        WHERE topic = 'time'
+                        ORDER BY datestamp DESC
+                        """)
+                        
+        try:
+            data = self.c.fetchone()[1:]
+        except TypeError as err:
+            data = None
+            print(err)
+
+        return data
+
+    def log_led(self):
+        print("2-color single LED log")
+        self.c.execute("SELECT * FROM data WHERE topic = 'led'")
+        for row in self.c.fetchall():
+            print(row)
+        
+    def log_temperature(self):
+        print("Temperature sensor log")
+        self.c.execute("SELECT * FROM data WHERE topic = 'temperature'")
+        for row in self.c.fetchall():
+            print(row)
 
     def log_sound(self):
         print("Sound sensor log")
-        self.c.execute("SELECT * FROM database WHERE topic = 'sound'")
+        self.c.execute("SELECT * FROM data WHERE topic = 'sound'")
         for row in self.c.fetchall():
             print(row)
 
     def log_light(self):
         print("Light sensor log:")
-        self.c.execute("SELECT * FROM database WHERE topic = 'light'")
+        self.c.execute("SELECT * FROM data WHERE topic = 'light'")
         for row in self.c.fetchall():
             print(row)
 
-    
+    def log_infrared(self):
+        print("Infrared sensor log")
+        self.c.execute("SELECT * FROM data WHERE topic = 'infrared'")
+        for row in self.c.fetchall():
+            print(row)
+
+    def log_time(self):
+        print("Real-time Clock log")
+        self.c.execute("SELECT * FROM data WHERE topic = 'time'")
+        for row in self.c.fetchall():
+            print(row)
